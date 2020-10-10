6cacb8a3521bebef92b3958a99c16902782f733b
andyjohn23 <andrewnick906@gmail.com>
2020-10-10 20:31:29 +0300

    updated user class

--- user.py
+++ user.py
@@ -1,14 +1,23 @@
+import pickle
+import random
+import string
+
 class Users:
     """A class that generates new instances of Users"""
-    users_list = [] #Empty Users list
+    users_list = []  # Empty Users list
 
     def __init__(self, username, email, password):
+        """instance variable unique to each instance"""
 
-     # instance variable unique to each instance
-      self.username = username #new username
-      self.email = email       #new emailaddress
-      self.password = password #new password
+        self.username = username  # new username
+        self.email = email  # new emailaddress
+        self.password = password  # new password
 
     def save_users_details(self):
+        """saving users details to the users list"""
 
-     """saving users details to the users list"""
+        Users.users_list.append(self)
+
+
+
+        
