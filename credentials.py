87b3eba26a0cadeb35084cd9cb8c7632823a27e1
andyjohn23 <andrewnick906@gmail.com>
2020-10-10 20:32:31 +0300

    added user credential class methods

---
+++ credentials.py
@@ -0,0 +1,51 @@
+import string
+import random
+from user import Users
+
+
+class user_credentials:
+    """class for storing user credentials"""
+
+    user_credential_list = []
+
+    @classmethod
+    def checking_user_details(cls, username, password):
+        """Class method to check if user details match"""
+        registered_users = ""
+        for user in Users.users_list:
+            if (user.username == username and user.password == password):
+                registered_users = user.email
+        return registered_users
+
+    def __init__(self, username, social_network, account_name, password):
+        self.username = username
+        self.social_network = social_network
+        self.social_network_username = account_name
+        self.password = password
+
+    def save_user_details(self):
+        """saving user information"""
+        Users.users_list.append(self)
+
+    def delete_user_details(self):
+        """deleting user infromation"""
+        user_credentials.user_credential_list.remove(self)
+
+    def automatic_generated_password(self, length=12, password_value=string.digits+string.punctuation):
+        """Function to generate a random password"""
+        random_password = random.choices(password_value, k=length)
+        generated_password = ''.join(random_password)
+
+        return generated_password
+
+    @classmethod
+    def display_user_credentials(username):
+        """Class method to show user details"""
+        user_credential_list = []
+        for user_details in user_credentials.user_credential_list:
+            if user_details.username == username:
+             user_credential_list.append(user_details)
+        return user_details
+
+        
+    
