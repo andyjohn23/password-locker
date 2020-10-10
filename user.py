5aabca379f45fe272d328da26ed97f6a8381ce7f
andyjohn23 <andrewnick906@gmail.com>
2020-10-10 13:27:08 +0300

    added users class

---
+++ user.py
@@ -0,0 +1,14 @@
+class Users:
+    """A class that generates new instances of Users"""
+    users_list = [] #Empty Users list
+
+    def __init__(self, username, email, password):
+
+     # instance variable unique to each instance
+      self.username = username #new username
+      self.email = email       #new emailaddress
+      self.password = password #new password
+
+    def save_users_details(self):
+
+     """saving users details to the users list"""
