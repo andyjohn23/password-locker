62cca2d4d7f26c3b99ba54319518b4cc007b595e
andyjohn23 <andrewnick906@gmail.com>
2020-10-11 12:32:49 +0300

    updated sign up and loginverification

--- run.py
+++ run.py
@@ -7,18 +7,44 @@ def create_user(username, email, password):
     new_user = Users(username, email, password)
     return new_user
 
+
 def save_user(user):
     """Function to save user"""
     Users.save_users_details(user)
+
 
 def delete_user(user):
     """Function to delete user"""
     user.delete_user()
 
-def check_existing_users(username,password):
+
+def check_existing_users(username, password):
     """Function that check if user already exists"""
     verify_user = user_credentials.checking_user_details(username, password)
     return verify_user
+
+
+def create_users_details(username, social_network, account_name, password):
+    """Function to create user details"""
+    new_user_details = user_credentials(
+        username, social_network, account_name, password)
+    return new_user_details
+
+
+def save_users_details(details):
+    """Function to save new user details"""
+    user_credentials.save_user_details(details)
+
+
+def display_user_details(username):
+    """Function to display user details"""
+    return user_credentials.display_user_details(username)
+
+
+def automatic_generated_password():
+    """Function for automatically generating passwords for users"""
+    generated_password = user_credentials.automatic_generated_password()
+    return generated_password
 
 
 def main():
@@ -37,26 +63,35 @@ def main():
         if option == "1":
             print("Enter Username")
             username = input()
+            print("________________________________________")
 
             print("Enter Emailaddress")
             email = input()
+            print("_________________________________________")
 
             print("Enter New Password")
             password = input()
-            print(" ")
+            save_user(create_user(username, email, password))
+            print("__________________________________________")
 
-            print(
-                f"{username},you have successfully created an account, select login option")
+            print(f"{username},you have successfully created an account, select login option")
 
         elif option == "2":
+            print(" ")
+            print("Please enter your details")
+            print("___________________________________________")
+
             print("Enter Emailaddress")
             email = input()
 
             print("Enter Password")
             password = input()
+            verifying_user = check_existing_users(username, password)
 
-            print(
-                f"{username}, welcome to python password locker")
+            if verifying_user == username:
+                print(" ")
+                print(f"{username}, welcome to python password locker")
+                print(" ")
 
 
 if __name__ == '__main__':
